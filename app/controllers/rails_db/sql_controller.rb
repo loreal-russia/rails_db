77417e48d61d8307090117620a106da4fc316f54
Evgeniy Rashchepkin <e.a.rashchepkin@gmail.com>
2026-06-05 13:29:09 +0500

    Fix CSV export

--- app/controllers/rails_db/sql_controller.rb
+++ app/controllers/rails_db/sql_controller.rb
@@ -10,12 +10,12 @@ module RailsDb
     end
 
     def csv
-      file = Tempfile.new('results', Rails.application.paths['tmp'].expanded)
+      file = Tempfile.new('results', Tempfile.new('results', Rails.root.join('tmp').to_s))
       @sql_query.write_csv(file)
       file.close
       send_file(file.path, type: 'text/csv; charset=utf-8; header=present', filename: 'results.csv')
     rescue StandardError => _e
-      file.close!
+      file&.close!
     end
 
     def xls
